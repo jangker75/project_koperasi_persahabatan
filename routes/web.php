f0cafa98fab690530f0b99f4caa22e7c6c500715
Syahrul Setiawan <syahrulsetiaw626@gmail.com>
2022-10-25 21:16:14 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -199,6 +199,7 @@ Route::group([
 
     // Datatables Route Start
     Route::get('datatables-order', [OrderController::class, 'getIndexDatatables'])->name('order.index.datatables');
+    Route::get('datatables-opname', [OpnameController::class, 'getIndexDatatables'])->name('opname.index.datatables');
     Route::get('datatables-product', [ProductController::class, 'getIndexDatatables'])->name('product.index.datatables');
     Route::get('datatables-employee-index', [EmployeeController::class, 'getIndexDatatables'])->name('employee.index.datatables');
     Route::get('datatables-ex-employee-index', [ExEmployeeController::class, 'getIndexDatatables'])->name('ex-employee.index.datatables');
